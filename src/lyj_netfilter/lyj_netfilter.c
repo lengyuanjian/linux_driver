49a26054624e21053545b67c18de8a9d1bb695c1
lengyuanjian <1181444531@qq.com>
2023-09-11 20:31:10 +0800

    提交传化文件

--- src/lyj_netfilter/lyj_netfilter.c
+++ src/lyj_netfilter/lyj_netfilter.c
@@ -11,42 +11,9 @@
 #include "to_string.h"
 
 static struct nf_hook_ops nfho;
-static char buff[2048]={};
 static char str_buf[2048] = {};
+
 #define str_to_ip(a, b, c, d) htonl((a << 24) | (b << 16) | (c << 8) | d)
-static void to_string(struct iphdr head) 
-{
-    char buff[1024] = {};
-    ip_to_string(buff, 2048,&head);
-    printk(KERN_INFO "strings ip: %s\n", buff);
-}
-static void tcp_hdr_to_string(const struct tcphdr *tcp) {
-    
-    snprintf(buff,2048,"-Source Port: %u\n"
-           "Destination Port: %u\n"
-           "Sequence Number: %u\n"
-           "Acknowledgment Number: %u\n"
-           "Data Offset: %u\n"
-           "Flags: [FIN:%u, SYN:%u, RST:%u, PSH:%u, ACK:%u, URG:%u]\n"
-           "Window Size: %u\n"
-           "Checksum: 0x%04X\n"
-           "Urgent Pointer: %u\n",
-           ntohs(tcp->source),
-           ntohs(tcp->dest),
-           ntohl(tcp->seq),
-           ntohl(tcp->ack_seq),
-           tcp->doff,
-           tcp->fin,
-           tcp->syn,
-           tcp->rst,
-           tcp->psh,
-           tcp->ack,
-           tcp->urg,
-           ntohs(tcp->window),
-           ntohs(tcp->check),
-           ntohs(tcp->urg_ptr));
-           printk(KERN_INFO "%s\n", buff);
-}
 
 static unsigned int capture_packet(const struct nf_hook_ops *ops,
 			       struct sk_buff *skb,
@@ -59,52 +26,46 @@ static unsigned int capture_packet(const struct nf_hook_ops *ops,
 #endif
 			       )
 {
+    struct ethhdr *mac_header;
     struct iphdr *ip_header;
     struct tcphdr* tcp_header;
-    static int cout=0;
-    int tcp_data_len = 0;
-    int i = 0;
+    struct udphdr* udp_header;
+    int pos = 0;
     
-    /* Check if the packet is an IP packet */
-    if (skb->protocol == htons(ETH_P_IP) ) {
-        /* Get a pointer to the IP header */
+    if (skb->protocol == htons(ETH_P_IP) ) 
+    {
+        mac_header = (struct ethhdr *)skb_mac_header(skb);
         ip_header = (struct iphdr *)skb_network_header(skb);
+        str_buf[0] = 0;
+        pos = 0;
+        pos += mac_to_string(str_buf + pos, 2048 - pos, mac_header);
+        str_buf[pos++] = '\n';
+        pos += ip_to_string(str_buf + pos, 2048 - pos, ip_header);
+        str_buf[pos++] = '\n';
         if(ip_header->protocol == 6)
         {
             tcp_header = (struct tcphdr*)skb_transport_header(skb);
-            //if(ntohs(tcp_header->source) == 12345 || ntohs(tcp_header->dest) == 12345)
+            if(ntohs(tcp_header->source) != 22 && ntohs(tcp_header->dest) != 22)
             {
-                printk(KERN_INFO "****seq:%d************\n", cout++);
-                printk(KERN_INFO "Captured packet: %pI4 -> %pI4  len:%d\n", 
-                    &ip_header->saddr, &ip_header->daddr,cpu_to_be16(ip_header->tot_len));
-                to_string(*ip_header);
-                tcp_hdr_to_string(tcp_header);
-                tcp_data_len = ntohs(ip_header->tot_len) - ip_header->ihl * 4 - tcp_header->doff * 4;
-                
-                for(i = 0; i < tcp_data_len; ++i)
-                {
-                    str_buf[i] = ((char *)(tcp_header))[tcp_header->doff * 4 + i];
-                    str_buf[i+1] = 0;
-                }
-                printk(KERN_INFO "data:len[%d][%s]\n", tcp_data_len, str_buf);
-                str_buf[0] = 0;
- 
-                printk(KERN_INFO "[%d]mac[%08x] ip[%08x] tcp[%08x] head[%08x]data[%08x]\n",(skb->len), skb_mac_header(skb),skb_network_header(skb),skb_transport_header(skb),
-                skb->head, skb->data);
-                printk(KERN_INFO "len[%d] mac_len[%d] hdr_len[%d] data_len[%d]\n",skb->len, skb->mac_len, skb->hdr_len, skb->data_len);
-                 printk(KERN_INFO "[%d][%d][%d] tail[%d]end[%d]\n",    
-                    skb->transport_header,
-				    skb->network_header,
-				    skb->mac_header,
-                    skb->tail,
-                    skb->end
-                    );
-			 
+                pos += tcp_to_string(str_buf + pos, 2048 - pos, tcp_header);
+                str_buf[pos++] = '\n';
+                str_buf[pos] = 0;
+                printk(KERN_INFO "%s", str_buf);
+            }
+        }
+        else if(ip_header->protocol == 0x11)
+        {
+            udp_header = (struct udphdr*)skb_transport_header(skb);
+            if(ntohs(udp_header->source) != 22 && ntohs(udp_header->dest) != 22)
+            {
+                pos += udp_to_string(str_buf + pos, 2048 - pos, udp_header);
+                str_buf[pos++] = '\n';
+                str_buf[pos] = 0;
+                printk(KERN_INFO "%s", str_buf);
             }
         }
     }
-    
-    /* Continue processing the packet */
+ 
     return NF_ACCEPT;
 }
 
--- src/lyj_netfilter/to_string.h
+++ src/lyj_netfilter/to_string.h
@@ -2,5 +2,11 @@
 #define _TO_STRING_H_
 #include <linux/ip.h>
 #include <linux/tcp.h>
-extern void ip_to_string(char * buf, const int len,const struct iphdr * head);
+#include <linux/if_ether.h>
+#include <linux/udp.h>
+extern int mac_to_string(char * buf, const int size, const struct ethhdr * head);
+extern int ip_to_string(char  * buf, const int size, const struct iphdr  * head);
+extern int tcp_to_string(char * buf, const int size, const struct tcphdr * head);
+extern int udp_to_string(char * buf, const int size, const struct udphdr * head);
+
 #endif
